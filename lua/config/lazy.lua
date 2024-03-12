fd1739cea28f2b646a639c9956f5c1b380481c7e
hxsggsz <vt.hugo.2021@gmail.com>
2024-03-12 12:14:57 -0300

    none-ls lib thinks its a good idea remove all linters deprecated and now I hate then

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,18 +3,36 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
+
       opts.sources = vim.list_extend(opts.sources, {
-        -- nls.builtins.diagnostics.sylelint,
         nls.builtins.formatting.stylelint,
         nls.builtins.diagnostics.actionlint,
         nls.builtins.diagnostics.yamllint,
         nls.builtins.formatting.gofmt,
         nls.builtins.formatting.prettierd,
         nls.builtins.formatting.goimports,
-        nls.builtins.code_actions.gomodifytags,
-        nls.builtins.code_actions.eslint_d,
-        -- nls.builtins.linting.eslint_d,
-        nls.builtins.diagnostics.eslint_d,
+        nls.builtins.diagnostics.eslint_d.with({
+          name = "eslint_d",
+          meta = {
+            url = "https://github.com/mantoni/eslint_d.js/",
+            description = "Like ESLint, but faster.",
+            notes = {
+              "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
+            },
+          },
+          command = "eslint_d",
+        }),
+        nls.builtins.code_actions.eslint_d.with({
+          name = "eslint_d",
+          command = "eslint_d",
+          meta = {
+            url = "https://github.com/mantoni/eslint_d.js",
+            description = "Injects actions to fix ESLint issues or ignore broken rules. Like ESLint, but faster.",
+            notes = {
+              "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
+            },
+          },
+        }),
         nls.builtins.code_actions.impl,
         nls.builtins.formatting.gofumpt,
         nls.builtins.diagnostics.golangci_lint,
