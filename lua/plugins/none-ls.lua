3b6be18519266504d568a5f82e43d1c4adc37fab
hxsggsz <vt.hugo.2021@gmail.com>
2024-03-15 10:26:02 -0300

    add eslint diagnostics and code actions again

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,41 +1,28 @@
 return {
   {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+      -- Adding this as a dependency because some of the default lsps were removed
+      -- See https://github.com/nvimtools/none-ls.nvim/discussions/81 for more information
+      "nvimtools/none-ls-extras.nvim",
+    },
     opts = function(_, opts)
       local nls = require("null-ls")
 
       opts.sources = vim.list_extend(opts.sources, {
         nls.builtins.formatting.stylelint,
+        nls.builtins.formatting.prettierd,
+        nls.builtins.formatting.goimports,
+        nls.builtins.formatting.gofumpt,
+
         nls.builtins.diagnostics.actionlint,
         nls.builtins.diagnostics.yamllint,
         nls.builtins.formatting.gofmt,
-        nls.builtins.formatting.prettierd,
-        nls.builtins.formatting.goimports,
-        nls.builtins.diagnostics.eslint_d.with({
-          name = "eslint_d",
-          meta = {
-            url = "https://github.com/mantoni/eslint_d.js/",
-            description = "Like ESLint, but faster.",
-            notes = {
-              "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
-            },
-          },
-          command = "eslint_d",
-        }),
-        nls.builtins.code_actions.eslint_d.with({
-          name = "eslint_d",
-          command = "eslint_d",
-          meta = {
-            url = "https://github.com/mantoni/eslint_d.js",
-            description = "Injects actions to fix ESLint issues or ignore broken rules. Like ESLint, but faster.",
-            notes = {
-              "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
-            },
-          },
-        }),
-        nls.builtins.code_actions.impl,
-        nls.builtins.formatting.gofumpt,
         nls.builtins.diagnostics.golangci_lint,
+        require("none-ls.diagnostics.eslint_d"),
+
+        require("none-ls.code_actions.eslint_d"),
+        nls.builtins.code_actions.impl,
       })
     end,
   },
